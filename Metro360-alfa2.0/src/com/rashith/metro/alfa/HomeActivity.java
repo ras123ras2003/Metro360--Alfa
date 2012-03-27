58e6d2403483bef17405b17099b98838950449ee
Rashith Jayasekara <ras123ras2003@gmail.com>
2012-03-27 20:00:44 +0530

    added AR view

--- Metro360-alfa2.0/src/com/rashith/metro/alfa/HomeActivity.java
+++ Metro360-alfa2.0/src/com/rashith/metro/alfa/HomeActivity.java
@@ -1,4 +1,5 @@
 package com.rashith.metro.alfa;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -6,23 +7,22 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageButton;
 
-
 public class HomeActivity extends Activity {
 
-	//test for second
+	// test for second
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.home);
-		
+
 		final ImageButton ib = (ImageButton) findViewById(R.id.imageButton1);
 		ib.setOnClickListener(new OnClickListener() {
-			
-			
+
 			public void onClick(View v) {
 
-				Intent intent = new Intent(HomeActivity.this, ARViewActivity.class);
+				Intent intent = new Intent(HomeActivity.this,
+						RashARActivity.class);
 				startActivity(intent);
 			}
 		});
