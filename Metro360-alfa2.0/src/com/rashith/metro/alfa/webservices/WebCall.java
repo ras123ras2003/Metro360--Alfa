e84e62b9128574c7a4b21253e02b923235c122d6
Rashith Jayasekara <ras123ras2003@gmail.com>
2012-04-08 19:15:56 +0530

    desktop - webservice completed

--- Metro360-alfa2.0/src/com/rashith/metro/alfa/webservices/WebCall.java
+++ Metro360-alfa2.0/src/com/rashith/metro/alfa/webservices/WebCall.java
@@ -1,19 +1,23 @@
 package com.rashith.metro.alfa.webservices;
 
 import java.io.InputStream;
-
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.HttpsURLConnection;
-
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
+import org.apache.http.HttpVersion;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.params.ConnManagerPNames;
+import org.apache.http.conn.params.ConnPerRouteBean;
+import org.apache.http.conn.scheme.PlainSocketFactory;
 import org.apache.http.conn.scheme.Scheme;
 import org.apache.http.conn.scheme.SchemeRegistry;
 import org.apache.http.conn.ssl.SSLSocketFactory;
-import org.apache.http.conn.ssl.X509HostnameVerifier;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.impl.conn.SingleClientConnManager;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.params.HttpProtocolParams;
 
 import android.util.Log;
 
@@ -23,36 +27,41 @@ public class WebCall {
 
 	public static String getPlaces(double radious) {
 
-//		HostnameVerifier hostnameVerifier = org.apache.http.conn.ssl.SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER;
+//		SchemeRegistry schemeRegistry = new SchemeRegistry();
+//		schemeRegistry.register(new Scheme("http", PlainSocketFactory
+//				.getSocketFactory(), 80));
+//		schemeRegistry.register(new Scheme("mxiss", SSLSocketFactory
+//				.getSocketFactory(), 443));
 //
-//		DefaultHttpClient client = new DefaultHttpClient();
+//		HttpParams params = new BasicHttpParams();
+//		int timeoutConnection = 5000;
+//		HttpConnectionParams.setConnectionTimeout(params, timeoutConnection);
+//		int timeoutSocket = 10000;
+//		HttpConnectionParams.setSoTimeout(params, timeoutSocket);
+//		params.setParameter(ConnManagerPNames.MAX_TOTAL_CONNECTIONS, 30);
+//		params.setParameter(ConnManagerPNames.MAX_CONNECTIONS_PER_ROUTE,
+//				new ConnPerRouteBean(30));
+//		params.setParameter(HttpProtocolParams.USE_EXPECT_CONTINUE, false);
+//		HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
 //
-//		SchemeRegistry registry = new SchemeRegistry();
-//		SSLSocketFactory socketFactory = SSLSocketFactory.getSocketFactory();
-//		socketFactory
-//				.setHostnameVerifier((X509HostnameVerifier) hostnameVerifier);
-//		registry.register(new Scheme("https", socketFactory, 443));
-//		SingleClientConnManager mgr = new SingleClientConnManager(
-//				client.getParams(), registry);
-//		DefaultHttpClient httpClient = new DefaultHttpClient(mgr,
-//				client.getParams());
-//
-//		// Set verifier
-//		HttpsURLConnection.setDefaultHostnameVerifier(hostnameVerifier);
+//		ThreadSafeClientConnManager cm = new ThreadSafeClientConnManager(
+//				params, schemeRegistry);
 
-		
-		
-		// String uri =
-		// "https://maps.googleapis.com/maps/api/place/search/json?location=6.969576,79.909327&radius=500&sensor=false&key=AIzaSyBYApAXNOVnGRjPjwaP_VasuWTVfTyf7T8";
-		String uri = "http://www.webservicex.net/periodictable.asmx?WSDL";
+		String uri = "https://maps.googleapis.com/maps/api/place/search/json?location=6.969576,79.909327&radius=500&sensor=false&key=AIzaSyBYApAXNOVnGRjPjwaP_VasuWTVfTyf7T8";
+		//String uri = "http://www.webservicex.net/periodictable.asmx?WSDL";
+
 		Log.d(tag, uri);
 
-		DefaultHttpClient client = new DefaultHttpClient();
+		HttpClient client = new DefaultHttpClient();
 		HttpPost post = new HttpPost(uri);
 
 		HttpResponse response = null;
 
 		try {
+			// URL u = new URL(uri);
+			// HttpsURLConnection posts = (HttpsURLConnection)
+			// u.openConnection();
+			// InputStream i = posts.getInputStream();
 
 			response = client.execute(post);
 			int statusCode = response.getStatusLine().getStatusCode();
